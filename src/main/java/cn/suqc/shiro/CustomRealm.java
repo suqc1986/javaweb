ce96e2d22f7d536bc3dc6d6d388f813d51d16913
sqc <sqc1986@163.com>
2018-04-05 15:35:24 +0800

    1.学习shiro框架,控制系统权限

---
+++ src/main/java/cn/suqc/shiro/CustomRealm.java
@@ -0,0 +1,64 @@
+package cn.suqc.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomRealm extends AuthorizingRealm {
+    /***
+     * 认证
+     * @param token
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        //从token中获取用户身份信息
+        String username = (String)token.getPrincipal();
+        //通过username从数据库中查询
+        //如果查询不到则返回null
+        if(!username.equals("zhangsan")){//这里模拟查询不到
+            return null;
+        }
+        //获取数据库查询出来的用户密码
+        String dbPassword = "zhangsan";
+        //返回认证信息由父类AuthenticatingRealm进行认证
+        SimpleAuthenticationInfo simpleAuthenticationInfo =
+                new SimpleAuthenticationInfo(username,dbPassword,getName());
+        return simpleAuthenticationInfo;
+    }
+
+    /***
+     * 授权
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        // 获取身份信息
+        String username = (String) principals.getPrimaryPrincipal();
+        // 根据身份信息从数据库中查询权限数据
+        // 这里使用静态数据模拟
+        List<String> permissions = new ArrayList<String>();
+        permissions.add("user:*");
+        permissions.add("department:*");
+        // 将权限信息封闭为AuthorizationInfo
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        // 模拟数据，添加 manager 角色
+        simpleAuthorizationInfo.addRole("manager");
+
+        for(String permission:permissions){
+            simpleAuthorizationInfo.addStringPermission(permission);
+        }
+
+        return simpleAuthorizationInfo;
+    }
+}
--- src/main/webapp/WEB-INF/views/welcome.jsp
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -1,5 +1,6 @@
 <%@ page import="java.util.Date" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="shiro" uri="http://shiro.apache.org/tags" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
 <head>
@@ -8,5 +9,11 @@
 <body>
 <c:set var="pageName" value="welcome.jsp"/>
     ${pageName.substring(0,pageName.length()).toUpperCase()} <%=new Date().toLocaleString()%>
+    <shiro:hasPermission name="user:manager">
+        <a href="http://www.baidu.com">点击进入管理界面</a>
+    </shiro:hasPermission>
+    <shiro:lacksPermission name="user:manage">
+        没有管理权限
+    </shiro:lacksPermission>
 </body>
 </html>
