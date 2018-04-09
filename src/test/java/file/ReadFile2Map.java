3ae094683b18c8cd6729c4e5494b81ad10ddcdd9
sqc <sqc1986@163.com>
2018-04-09 11:23:51 +0800

    1.调整国际化资源文件转化代码

--- src/test/java/file/ReadFile2Map.java
+++ src/test/java/file/ReadFile2Map.java
@@ -12,27 +12,49 @@ import java.util.Set;
 
 public class ReadFile2Map {
     public static void main(String[] args) throws Exception{
-        Map<String,String> resultMap = Maps.newHashMap();
-        Map<String,String> enMap = File2Map("C:\\Users\\suqc\\Desktop\\机构知识库中英文翻译 20180404.txt","gbk");
-        System.out.println(enMap);
-        Map<String,String> resMap = File2Map("C:\\Users\\suqc\\Desktop\\备份\\new 2.properties","utf8");
-        System.out.println(resMap);
-        System.out.println(enMap.size());
-        System.out.println(resMap.size());
-         for(Iterator<String> ite = resMap.keySet().iterator();ite.hasNext();){
-             String key = ite.next();
-//             System.out.println(key+":"+resMap.get(key)+":");
-             if(enMap.keySet().contains(resMap.get(key))){
-                 resultMap.put(key,enMap.get(resMap.get(key)));
+        Map<String,String> matchOkMap = Maps.newHashMap();
+        Map<String,String> matchFailMap = Maps.newHashMap();
+        //解析英文对照文件
+        Map<String,String> enResMap = File2Map("C:\\Users\\suqc\\Desktop\\机构知识库中英文翻译 20180404.txt","gbk");
+        //解析中文对照文件
+        Map<String,String> zhResMap = File2Map("C:\\Users\\suqc\\Desktop\\备份\\new 2.properties","utf8");
+        System.out.println("英文资源数:"+enResMap.size());
+        System.out.println("中文资源数:"+zhResMap.size());
+        //
+         for(Iterator<String> ite = zhResMap.keySet().iterator();ite.hasNext();){
+             String resKey = ite.next();
+             if(enResMap.keySet().contains(zhResMap.get(resKey))){
+                 //匹配成功
+                 matchOkMap.put(resKey,enResMap.remove(zhResMap.get(resKey)));
+             }else{
+                //未匹配成功
+                 matchFailMap.put(resKey,zhResMap.get(resKey));
              }
-
          }
-         Set<Map.Entry<String,String>> entrySet = resultMap.entrySet();
-        System.out.println(entrySet.size());
+
+         printResult(matchOkMap);
+         printResult(matchFailMap);
+         printResult(enResMap);
+    }
+
+    /**
+     * 打印结果
+     * @param map
+     */
+    public static void printResult(Map<String,String> map){
+        System.out.println("-------------------------------------打印统计----------------------------------->"+map.size()+"条");
+        Set<Map.Entry<String,String>> entrySet = map.entrySet();
         for(Map.Entry<String,String> entry:entrySet){
             System.out.println(entry.getKey()+"="+entry.getValue());
         }
     }
+    /**
+     * 读取文件转成Map
+     * @param path
+     * @param charsetName
+     * @return
+     * @throws Exception
+     */
     public static Map<String,String> File2Map(String path,String charsetName) throws Exception{
         Map<String,String> map = Maps.newHashMap();
         FileInputStream fis = new FileInputStream(path);
@@ -51,6 +73,5 @@ public class ReadFile2Map {
         bif.close();
         fis.close();
         return map;
-
     }
 }
