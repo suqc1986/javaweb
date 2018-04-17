43d992b9f4e326475e1e3797f9ba5aed2d2608c1
sqc <sqc1986@163.com>
2018-04-17 13:32:04 +0800

    1.多线程访问临界资源.

---
+++ src/test/java/thread/Main.java
@@ -0,0 +1,40 @@
+package thread;
+
+public class Main {
+    public static void main(String[] args){
+        UpdateService us = new UpdateService();
+        Thread t1 = new UpdateThread(us);
+        Thread t2 = new UpdateThread(us);
+        Thread t3 = new UpdateThread(us);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+class UpdateThread extends Thread{
+    UpdateService us;
+    public UpdateThread(UpdateService us){
+        this.us = us;
+    }
+    @Override
+    public void run() {
+        while(true){
+            int page = us.getPage();
+            if(page > 10)break;
+            System.out.println(Thread.currentThread().getName()+"----> page:"+page);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class UpdateService{
+    private int page = 1;
+    public synchronized   int getPage(){
+        return page++;
+    }
+}
